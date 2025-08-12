6ad83c598fdd5eeb80ebab49d6789ad64eafe194
Mike <mclancy96@gmail.com>
2025-08-12 13:57:11 -0400

    Make test more resilient

--- spec/sinatra_basic_forms_lab_spec.rb
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Rails Forms Lab', type: :feature do
 
     it 'renders welcome' do
       visit '/'
-      expect(page).to have_link("Click Here To List A Puppy")
+      expect(page).to have_link('Click Here To List A Puppy')
     end
   end
 
@@ -21,7 +21,7 @@ RSpec.describe 'Rails Forms Lab', type: :feature do
 
     it 'renders the form' do
       visit '/new'
-      expect(page).to have_selector("form")
+      expect(page).to have_selector('form')
       expect(page).to have_field(:name)
       expect(page).to have_field(:breed)
       expect(page).to have_field(:age)
@@ -29,21 +29,21 @@ RSpec.describe 'Rails Forms Lab', type: :feature do
   end
 
   describe 'POST /puppy' do
-    it "displays the puppy" do
+    it 'displays the puppy' do
       visit '/new'
 
-      fill_in(:name, :with => "Butch")
-      fill_in(:breed, :with => "Mastiff")
-      fill_in(:age, :with => "6 months")
-      click_button "submit"
+      fill_in(:name, with: 'Butch')
+      fill_in(:breed, with: 'Mastiff')
+      fill_in(:age, with: '6 months')
+      click_button 'submit'
 
       # Check that the page contains the puppy information
-      expect(page).to have_text("Puppy Name")
-      expect(page).to have_text("Butch")
-      expect(page).to have_text("Puppy Breed")
-      expect(page).to have_text("Mastiff")
-      expect(page).to have_text("Puppy Age")
-      expect(page).to have_text("6 months")
+      expect(page).to have_text('Puppy Name')
+      expect(page).to have_text('Butch')
+      expect(page).to have_text('Puppy Breed')
+      expect(page).to have_text('Mastiff')
+      expect(page).to have_text('Puppy Age')
+      expect(page).to have_text('6 months')
     end
   end
 end
