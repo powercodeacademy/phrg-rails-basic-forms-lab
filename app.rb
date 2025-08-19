71cfbd073f349d09460ccacf568748ec9567c43c
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-19 10:16:17 -0400

    Remove sinatra references

--- app.rb
+++
@@ -1,2 +0,0 @@
-# This file is no longer needed in Rails.
-# The functionality has been moved to app/controllers/puppies_controller.rb
--- spec/models/puppy_spec.rb
+++ spec/models/puppy_spec.rb
@@ -1,35 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Puppy, type: :model do
-  let!(:puppy) { Puppy.new("brad", "black lab", 2) }
+  let!(:puppy) { Puppy.new(name: "brad", breed: "black lab", age: 2) }
 
-  it 'can create a new instance of the puppy class' do
-    expect(Puppy.new("brad", "black lab", 2)).to be_an_instance_of(Puppy)
+  it "can create a new instance of the puppy class" do
+    expect(Puppy.new(name: "brad", breed: "black lab", age: 2)).to be_an_instance_of(Puppy)
   end
 
-  it 'can read a puppy name' do
+  it "can read a puppy name" do
     expect(puppy.name).to eq("brad")
   end
 
-  it 'can read a puppy breed' do
+  it "can read a puppy breed" do
     expect(puppy.breed).to eq("black lab")
   end
 
-  it 'can read a puppy age' do
+  it "can read a puppy age" do
     expect(puppy.age).to eq(2)
   end
 
-  it 'can change puppy age' do
+  it "can change puppy age" do
     puppy.age = 3
     expect(puppy.age).to eq(3)
   end
 
-  it 'can change puppy name' do
+  it "can change puppy name" do
     puppy.name = "brad the beast"
     expect(puppy.name).to eq("brad the beast")
   end
 
-  it 'can change puppy breed' do
+  it "can change puppy breed" do
     puppy.breed = "the best black lab"
     expect(puppy.breed).to eq("the best black lab")
   end
