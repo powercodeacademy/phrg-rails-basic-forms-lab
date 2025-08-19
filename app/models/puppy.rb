3f3f708b817328b05bec7de2aaa6b0cf25a0ce50
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-19 09:33:36 -0400

    Adjust to pass tests

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -1,9 +1,14 @@
-class Puppy < ApplicationRecord 
-  def self.new(name = nil, breed = nil, age = nil, **attrs)
-    if name && breed && age
-      super(attrs.merge(name: name, breed: breed, age: age))
-    else
-      super
+class Puppy < ApplicationRecord
+  class << self
+    alias_method :ar_new, :new  
+
+    def new(*args, **attrs)
+      if args.size == 3
+        name, breed, age = args
+        ar_new(attrs.merge(name: name, breed: breed, age: age))
+      else
+        ar_new(*args, **attrs)
+      end
     end
   end
 end
