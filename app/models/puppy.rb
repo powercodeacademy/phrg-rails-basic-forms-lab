29c04c3f5f2731d4f0ae41f7b81a600a6850cc49
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-19 09:12:14 -0400

    Set up initializer to pass tests

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -1,10 +1,9 @@
-# TODO: Build out your Puppy class here
-# Your puppy should have name, breed, and age attributes
-# You will need to be able to pass these three attributes to initialization
-# as well as readers and writers for the attributes
-
-class Puppy
-  # TODO: Add your code here
-  # Hint: You'll need attr_accessor for name, breed, and age
-  # You'll also need an initialize method that takes name, breed, and age as parameters
+class Puppy < ApplicationRecord 
+  def self.new(name = nil, breed = nil, age = nil, **attrs)
+    if name && breed && age
+      super(attrs.merge(name: name, breed: breed, age: age))
+    else
+      super
+    end
+  end
 end
