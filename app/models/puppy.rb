c4c3d5be0930352596e487f6fa5d864ef4086b5a
Mike <mclancy96@gmail.com>
2025-08-20 10:24:09 -0400

    Setup routes and welcome page

--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -7,4 +7,11 @@ class Puppy
   # TODO: Add your code here
   # Hint: You'll need attr_accessor for name, breed, and age
   # You'll also need an initialize method that takes name, breed, and age as parameters
+  attr_accessor :name, :breed, :age
+
+  def initialize(name:, breed:, age:)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 end
