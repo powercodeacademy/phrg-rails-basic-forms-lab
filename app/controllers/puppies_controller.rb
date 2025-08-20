874ae997cc0dc3740c2f94629867cdafed2ceae7
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-19 23:11:10 -0400

    Write index, new, create actions and create routes

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,6 +1,21 @@
 class PuppiesController < ApplicationController
-  # TODO: Add your controller actions here
-  # You'll need an 'index' action to display the homepage
-  # You'll need a 'new' action to display the form
-  # You'll need a 'create' action to process the form submission and display the puppy
+  def index
+    @puppies = Puppy.all
+  end
+
+  def new
+    @puppy = Puppy.new
+  end
+
+  def create
+    @puppy = Puppy.create(puppy_params)
+    @puppy.save
+    redirect_to puppy_path(@puppy)
+  end
+
+  private
+
+  def puppy_params
+    params.require(:puppy).permit(:name, :breed, :age)
+  end
 end
