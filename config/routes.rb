e71c56e2dc9f39aebb0da9cd47d9c34680e3c79f
Shantel <Shantelgray24@gmail.com>
2025-08-20 14:45:17 -0400

    Cleaning up sudo code

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,4 @@ Rails.application.routes.draw do
   get '/puppies/new', to: 'puppies#new'
   post '/puppies', to: 'puppies#create'
   get '/puppies/:id', to: 'puppies#show'
-  # TODO: Add your routes here
-  # You need to use RESTful routing conventions
-  # Hint: Use the resources method to create all the standard RESTful routes
-  # This will create routes for index, show, new, create, edit, update, and destroy
 end
