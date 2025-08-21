3efb8270181d927f1b73bb12c3eaa51ce38dab46
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-21 09:05:38 -0400

    Display all puppies

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,9 +1,5 @@
 class PuppiesController < ApplicationController
-  # TODO: Add your controller actions here
-  # You'll need an 'index' action to display the homepage
-  # You'll need a 'new' action to display the form
-  # You'll need a 'create' action to process the form submission and redirect to show
-  # You'll need a 'show' action to display the puppy information
-  #
-  # Remember: After creating a puppy, you should redirect to the show page
+  def index
+    @puppies = Puppy.all
+  end
 end
