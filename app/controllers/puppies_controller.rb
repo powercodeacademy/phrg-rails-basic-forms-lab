19eb65528eeda1e1c7ee9c45d10543583342c468
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-21 09:32:59 -0400

    Create and display puppy on form submission

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -3,7 +3,22 @@ class PuppiesController < ApplicationController
     @puppies = Puppy.all
   end
 
+  def show
+    @puppy = Puppy.find(params[:id])
+  end
+
   def new
     @puppy = Puppy.new
+  end
+
+  def create
+    puppy = Puppy.create!(puppy_params)
+    redirect_to puppy_path(puppy)
+  end
+
+  private
+
+  def puppy_params
+    params.require(:puppy).permit(:name, :breed, :age)
   end
 end
